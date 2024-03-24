65b32410cf98c0c3e947246b9a970007e60b5074
xuanwenj <139101662+xuanwenj@users.noreply.github.com>
2024-03-24 21:22:57 +1300

    Label, frame widgets

--- practice1.py
+++ practice1.py
@@ -1,9 +1,20 @@
+import tkinter
+
 def hello():
     print("Hello World")
 
-import tkinter
 window = tkinter.Tk()
-button = tkinter.Button(window, text = 'Hello World', command = hello)
+window.title("Tkinter App")
+window.geometry('600x400')
+# window is the parent of the label
+
+frame = tkinter.Frame(window)
+frame.pack()
+label = tkinter.Label(frame, text = "Label")
+label.pack()
+button = tkinter.Button(frame, text = 'Hello World', command = hello)
 button.pack()
+print("hi")
 
 window.mainloop()
+
