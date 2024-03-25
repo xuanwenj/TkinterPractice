c08548b2a7e53e9b77160099b82cc83e8ee7049d
xuanwenj <139101662+xuanwenj@users.noreply.github.com>
2024-03-25 16:04:12 +1300

    Text entry

--- practice1.py
+++ practice1.py
@@ -1,20 +1,26 @@
 import tkinter
 
-def hello():
-    print("Hello World")
+
 
 window = tkinter.Tk()
+def hello():
+    print(textentry.get())    
+
 window.title("Tkinter App")
 window.geometry('600x400')
 # window is the parent of the label
 
 frame = tkinter.Frame(window)
 frame.pack()
-label = tkinter.Label(frame, text = "Label")
+label = tkinter.Label(frame, text = "Label", bg = "purple", fg = "white", width = 50)
 label.pack()
-button = tkinter.Button(frame, text = 'Hello World', command = hello)
+textentry = tkinter.Entry(window, bg = "light green", width = 50, show="*")
+textentry.pack()
+
+button = tkinter.Button(frame, text = 'Hello World', bg = "#fdbce1", activebackground= "red", command = hello)
 button.pack()
 print("hi")
+
 
 window.mainloop()
 
